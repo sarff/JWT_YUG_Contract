68efbf5b9c640dd7d0f17aa96a937c5cdff50792
itdim <sarff@mail.ru>
2022-02-13 21:16:36 +0200

    fix filters from env

--- requests.go
+++ requests.go
@@ -23,12 +23,11 @@ func getPrice(TokenBraer string) {
 	method := "POST"
 
 	payload := strings.NewReader(`{
-  "format": "json",
-  "type": "regular",
-  "cats": [498,177,552,196,1463,197,1217,1215,85,218,129,494,142,493,98,99,102,103,1247,560,1421,492,118,111,225,1514,106,1422,1037,1036,414,354,1460,367,372,601,345,347,1394,1448,1311,1438,1302,1449,1280,1211,1279,1450,1312,1456,1487,1512,1457,1301,404,392,1439,390,395,1446,254,252,1441,250,256,796,58,1187,56,51,50,233,244,231],
-  "ext_cols": ['artikul','descr','url','bigimg'],
-  "type_prod": "new"
-}`)
+  		"type": ` + goDotEnvVariable("TYPE_PROD") + `,
+  		"cats": [` + goDotEnvVariable("CATS") + `],
+  		"ext_cols": [` + goDotEnvVariable("EXT_COLS") + `],
+  		"type_prod": "new"
+		}`)
 
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, payload)
